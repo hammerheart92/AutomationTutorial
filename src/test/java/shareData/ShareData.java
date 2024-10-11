fcc18198a612631a300f36807f080d699212e4a8
hammerheart92 <hammerheart92@gmail.com>
2024-10-11 14:41:17 +0300

    Small refactoring for ci/cd

--- src/test/java/shareData/ShareData.java
+++ src/test/java/shareData/ShareData.java
@@ -2,6 +2,7 @@ package shareData;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -10,15 +11,16 @@ public class ShareData {
     private WebDriver driver;
 
     public void setUpDriver(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new"); // Run in headless mode for CI
+        options.addArguments("--no-sandbox"); // Required for GitHub Actions
+        options.addArguments("--disable-dev-shm-usage"); // Prevents memory issues
+        options.addArguments("--window-size=1920,1080"); // Set a default window size
 
-        //Deschidem un browser
-        driver = new ChromeDriver();
-        //Accesam un url
+        driver = new ChromeDriver(options);
         driver.get("https://demoqa.com/");
-        //Facem browserul maximize
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
     }
 
     public WebDriver getDriver() {

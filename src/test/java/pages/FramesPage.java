952c643693d6d004935973a7b36e6ae6e3131db6
hammerheart92 <hammerheart92@gmail.com>
2025-06-21 12:53:28 +0300

    Fix iframe stability issues in CI: update Chrome options and add waits

--- src/test/java/pages/FramesPage.java
+++ src/test/java/pages/FramesPage.java
@@ -1,10 +1,10 @@
 package pages;
 
-import loggerUtility.LoggerUtility;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
+
+import loggerUtility.LoggerUtility;
+import org.openqa.selenium.WebDriver;
 
 public class FramesPage extends BasePage {
 
@@ -12,33 +12,32 @@ public class FramesPage extends BasePage {
         super(driver);
     }
 
-    @FindBy(id = "sampleHeading")
-    private WebElement sampleTextElement;
-    @FindBy(id = "sampleHeading")
-    private WebElement secondSampleTextElement;
+    private final By sampleHeadingLocator = By.id("sampleHeading");
 
-    public void dealFirstIFrame(){
+    public void dealFirstIFrame() {
         frameMethods.switchToSpecificIframe("frame1");
         LoggerUtility.infoLog("The user switches to the first iframe");
 
-        elementMethods.waitVisibleElement(sampleTextElement);
+        WebElement sampleHeading = driver.findElement(sampleHeadingLocator);
+        elementMethods.waitVisibleElement(sampleHeading);
         LoggerUtility.infoLog("The user waits for the element inside the first iframe");
 
-        System.out.println(sampleTextElement.getText());
+        System.out.println(sampleHeading.getText());
         LoggerUtility.infoLog("The user reads text inside the first iframe");
 
         frameMethods.switchToParentIframe();
         LoggerUtility.infoLog("The user switches back to the parent frame");
     }
 
-    public void dealSecondIFrame(){
+    public void dealSecondIFrame() {
         frameMethods.switchToSpecificIframe("frame2");
         LoggerUtility.infoLog("The user switches to the second iframe");
 
-        elementMethods.waitVisibleElement(sampleTextElement); // Reuse same locator, if valid
+        WebElement sampleHeading = driver.findElement(sampleHeadingLocator);
+        elementMethods.waitVisibleElement(sampleHeading);
         LoggerUtility.infoLog("The user waits for the element inside the second iframe");
 
-        System.out.println(sampleTextElement.getText());
+        System.out.println(sampleHeading.getText());
         LoggerUtility.infoLog("The user reads text inside the second iframe");
 
         frameMethods.switchToParentIframe();
